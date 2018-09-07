556781c1b8b6f713a6ecaebbab7100263575c539
BidyutPrava <bidyutprava.sbd@gmail.com>
2018-09-07 11:54:34 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,12 @@ import {NavItem} from './nav-item';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  collection = [];
+  constructor() {
+    // for (let i = 1; i <= 100; i++) {
+    //   this.collection.push(`item ${i}`);
+    // }
+  }
   title = 'website';
   version = VERSION;
   navItems: NavItem[] = [
